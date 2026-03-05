1a5eb394126abbdd26c7ed81909156a0e111f402
tootooluck-svg <tootooluck@gmail.com>
2026-03-05 14:42:36 +0800

    Add files via upload

--- eoraptor-website/frontend/next.config.ts
+++ eoraptor-website/frontend/next.config.ts
@@ -4,8 +4,6 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true
   }
